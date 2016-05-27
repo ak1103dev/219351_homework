b1869410413a41823f9ac4585954595c240ced13
ak1103dev <ak.apichan1103@gmail.com>
2016-05-27 11:09:38 +0700

    change main and merge

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -5,7 +5,6 @@
 
 #define N 100000
 
-
 void swap(int *xp, int *yp) {
   int temp = *xp;
   *xp = *yp;
@@ -21,31 +20,43 @@ void bubbleSort(int arr[], int n) {
 		swap(&arr[j], &arr[j+1]);
 }
 
-int *merge(int * v1, int n1, int * v2, int n2)
+int * merge(int *v1, int n1, int *v2, int n2)
 {
-  int * result = (int *)malloc((n1 + n2) * sizeof(int));
-  int i = 0;
-  int j = 0;
-  int k;
-  for (k = 0; k < n1 + n2; k++) {
-      if (i >= n1) {
-	        result[k] = v2[j];
-	        j++;
-	      }
-      else if (j >= n2) {
-	        result[k] = v1[i];
-	        i++;
-	      }
-      else if (v1[i] < v2[j]) { // indices in bounds as i < n1 && j < n2
-	        result[k] = v1[i];
-	        i++;
-	      }
-      else { // v2[j] <= v1[i]
-	        result[k] = v2[j];
-	        j++;
-	      }
-    }
-  return result;
+	int i,j,k;
+	int * result;
+
+	result = (int *)malloc((n1+n2)*sizeof(int));
+
+	/*
+		i : pointer of v1
+		j : pointer of v2
+		k : pointer of k
+	*/
+	i=0; j=0; k=0;
+	while(i<n1 && j<n2)
+		if(v1[i]<v2[j])
+		{
+			result[k] = v1[i];
+			i++; k++;
+		}
+		else
+		{
+			result[k] = v2[j];
+			j++; k++;
+		}
+	if(i==n1)
+		while(j<n2)
+		{
+			result[k] = v2[j];
+			j++; k++;
+		}
+	else
+		while(i<n1)
+		{
+			result[k] = v1[i];
+			i++; k++;
+		}
+	return result;
 }
 
 int isSorted(int *a, int size) {
@@ -69,51 +80,92 @@ void printArray(int arr[], int size)
 
 
 int main(int argc, char** argv) {
-	int i, n;
-	int* A = NULL;
+	int * data;
+	int * chunk;
+	int * other;
+	int m,n=N;
+	int id,p;
+	int s;
+	int i;
+	int step;
+	MPI_Status status;
+
+	MPI_Init(&argc,&argv);
+	MPI_Comm_rank(MPI_COMM_WORLD,&id);
+	MPI_Comm_size(MPI_COMM_WORLD,&p);
+
 	clock_t start, end;
 	double elapsed_time, t1, t2;
 
-	int rank, size;
-	int c;
-
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-	if(rank == 0) {
-		// compute chunk size
-		c = N/size;
-		if (N%size) c++;
-		// read data from file
-		A = (int *)malloc(size*c * sizeof(int));
-		if (A == NULL) {
-			printf("Fail to malloc\n");
-			exit(0);
+	if(id==0)
+	{
+		int r;
+		srandom(clock());
+		s = n/p;
+		r = n%p;
+		data = (int *)malloc((n+p-r)*sizeof(int));
+		for(i=0;i<n;i++)
+			data[i] = random();
+		if(r!=0)
+		{
+			for(i=n;i<n+p-r;i++)
+  			   data[i]=0;
+			s=s+1;
 		}
-		for (i=N-1; i>=0; i--)
-			A[N-1-i] = i;
-		// pad data with 0 -- doesn't matter
-		//for (i = n; i < p*c; i++)
-		 // data[i] = 0;
+
+		start = clock();
+
+		MPI_Bcast(&s,1,MPI_INT,0,MPI_COMM_WORLD);
+		chunk = (int *)malloc(s*sizeof(int));
+		MPI_Scatter(data,s,MPI_INT,chunk,s,MPI_INT,0,MPI_COMM_WORLD);
+
+		bubbleSort(chunk,s);
+	}
+	else
+	{
+		MPI_Bcast(&s,1,MPI_INT,0,MPI_COMM_WORLD);
+		chunk = (int *)malloc(s*sizeof(int));
+		MPI_Scatter(data,s,MPI_INT,chunk,s,MPI_INT,0,MPI_COMM_WORLD);
+
+		bubbleSort(chunk,s);
 	}
 
-	t1 = MPI_Wtime();
-	if (isSorted(A, N))
-	  printf("Array is sorted\n");
-	else
-	  printf("Array is NOT sorted\n");
+	step = 1;
+	while(step<p)
+	{
+		if(id%(2*step)==0)
+		{
+			if(id+step<p)
+			{
+				MPI_Recv(&m,1,MPI_INT,id+step,0,MPI_COMM_WORLD,&status);
+				other = (int *)malloc(m*sizeof(int));
+				MPI_Recv(other,m,MPI_INT,id+step,0,MPI_COMM_WORLD,&status);
+				chunk = merge(chunk,s,other,m);
+				s = s+m;
+			}
+		}
+		else
+		{
+			int near = id-step;
+			MPI_Send(&s,1,MPI_INT,near,0,MPI_COMM_WORLD);
+			MPI_Send(chunk,s,MPI_INT,near,0,MPI_COMM_WORLD);
+			break;
+		}
+		step = step*2;
+	}
+	if(id==0)
+	{
+		FILE * fout;
 
-	bubbleSort(A, N);
-	printArray(&A[N-10], 10);
+		end = clock();
+		printf("%d; %d processors; %f secs\n",N,p,(end-start)/CLOCKS_PER_SEC);
 
-	if (isSorted(A, N))
-	  printf("Array is sorted\n");
-	else
-	  printf("Array is NOT sorted\n");
-
-	t2 = MPI_Wtime();
-	printf( "Elapsed time MPI_Wtime is %f\n", t2 - t1 );
+		fout = fopen("result","w");
+		for(i=0;i<s;i++)
+		   if (chunk[i] != 0)
+			fprintf(fout,"%d\n",chunk[i]);
+		fclose(fout);
+	}
 	MPI_Finalize();
 	return 0;
 }
